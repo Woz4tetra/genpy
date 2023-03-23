b93084062a6359028543f19e9454f7c49eab1390
Neil Macintyre <6704857+neilmacintyre@users.noreply.github.com>
2023-03-23 08:59:15 -0400

    Expand keyword args

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
 setup(
-    {
-    'packages':'genpy',
-    'package_dir': {'': 'src'},
-    'requires':'genmsg'
-    } 
+    packages='genpy',
+    package_dir={'': 'src'},
+    requires='genmsg'
 )
