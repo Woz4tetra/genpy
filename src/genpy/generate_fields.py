c1742022539f331d3745c33ab1b3e2ef30402d32
Neil Macintyre <6704857+neilmacintyre@users.noreply.github.com>
2023-03-28 09:39:40 -0400

    correctly type unint8 and chars - fix logic

--- src/genpy/generate_fields.py
+++ src/genpy/generate_fields.py
@@ -11,15 +11,15 @@ def generate_fields(spec_names: List[str], spec_types: List[str] ) -> List[Tuple
         base_type, is_array, array_length = genmsg.msgs.parse_type(spec_type)
         if len(base_type.split('/')) > 1:
             base_type = '.'.join(base_type.split('/')[:-1]) + '_msg_' +  base_type.split('/')[-1]
-            # base_type = base_type.split('/')[-1]
+
         if base_type in PY_TYPE_STRINGS:
             base_type = PY_TYPE_STRINGS[base_type]
         
-        if base_type != 'unint8' or base_type != 'char':
+        if is_array and (base_type != 'unint8' or base_type != 'char'):
             # unint8 and chars are stored in as bytes so they can't be set to a fixed length type
-            if is_array and array_length:
+            if array_length:
                 format_spec_type_hint = f'Tuple[{", ".join([base_type]*array_length)}]'
-            elif is_array:
+            else:
                 format_spec_type_hint = f'List[{base_type}]'
         else:
             format_spec_type_hint = base_type
