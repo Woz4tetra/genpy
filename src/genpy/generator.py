5f16d3fefdf687db6bdbd443a920508dfeb325b8
Neil Macintyre <6704857+neilmacintyre@users.noreply.github.com>
2023-10-06 16:29:14 -0400

    dbging

--- src/genpy/generator.py
+++ src/genpy/generator.py
@@ -928,7 +928,6 @@ def msg_generator(msg_context, spec, search_path):
     :param buff: buffer, ``StringIO``
     \"\"\""""
     for y in serialize_fn_generator(msg_context, spec):
-        yield '    ' + f'{spec}'
         yield '    ' + y
     yield """
   def deserialize(self, str: str) -> \'%s\':
